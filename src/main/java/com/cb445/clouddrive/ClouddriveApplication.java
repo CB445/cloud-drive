6f4bb94307cdff4fa25d050e1c49f10d829a8f42
calum <calumbousfield@gmail.com>
2026-06-04 23:51:30 +0100

    Set up Spring Boot, PostgreSQL and Maven project

---
+++ src/main/java/com/cb445/clouddrive/ClouddriveApplication.java
@@ -0,0 +1,13 @@
+package com.cb445.clouddrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClouddriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClouddriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cb445/clouddrive/ClouddriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cb445.clouddrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClouddriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
